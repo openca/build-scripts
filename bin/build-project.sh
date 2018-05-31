c211fec89b789c9bf07914364329a2b52286e9c3
Massimiliano Pala <director@openca.org>
2018-05-31 16:13:09 -0600

    Added main option for using specific branches for repositories. Fixed usage.

--- bin/build-project.sh
+++ bin/build-project.sh
@@ -2,10 +2,18 @@
 
 function usage {
 	echo
-	echo "    USAGE: $0 <project> [ rel | snap ] <version> <option>"
+	echo "  OpenCA Build Scripts - v0.9.0"
+	echo "  (c) 2018 by Massimiliano Pala and OpenCA Labs"
+	echo "  All Rights Reserved"
+	echo ""
+	echo "    USAGE: $0 <project> [ rel | snap ] \\"
+	echo "                        <version> <option> <branch>"
 	echo
-	echo "  Where <command> is one of all $binlist src"
-	echo " $prj, $rel, $ver, $opt"
+	echo "  Where:"
+	echo "  <project> .........: Project's Name (e.g. libpki)"
+	echo "  <version> .........: Version of the project (e.g., 0.9.0)"
+	echo "  <option> ..........: One of 'src' 'all' 'rpm' 'bin'"
+	echo "  <branch> ..........: Repository's Branch to use for the build"
 	echo
 }
 
@@ -15,6 +23,7 @@ prj="$1"
 rel="$2"
 ver="$3"
 opt="$4"
+branch="$5"
 
 # if using github repos, please enable the following line
 suffix=-github
@@ -94,7 +103,7 @@ for i in $list ; do
 	scp $server:/repository/scripts/$filename "$tmpdir"
 	if [ -f "$tmpdir/$filename" ] ; then
 		chmod +x "$tmpdir/$filename"
-		"$tmpdir/$filename" "$prj" "$rel" "$ver"
+		"$tmpdir/$filename" "$prj" "$rel" "$ver" "$branch"
 	else
 		echo "ERROR: can not transfer $filename from server ($server)"
 	fi
